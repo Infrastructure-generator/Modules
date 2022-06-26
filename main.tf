e3ee780e36e4bf671e7280f7b2d471faca5f1efc
Teo Podobnik <teo.podobnik1234@gmail.com>
2022-06-26 23:26:17 +0200

    feat: Initial script generating one VM

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    lxd = {
+      source  = "terraform-lxd/lxd"
+      version = "1.7.2"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+}
+
+provider "lxd" {
+  generate_client_certificates = true
+  accept_remote_certificate    = true
+}
+
+data "template_file" "cloudinit_file" {
+  template = "${file("cloud-init.yml")}"
+}
+
+resource "lxd_network" "mynetwork" {
+  name = "mynetwork"
+  config = {
+    "ipv4.address" = "10.150.19.1/24"
+    "ipv4.nat"     = "true"
+    "ipv6.address" = "fd42:474b:622d:259d::1/64"
+    "ipv6.nat"     = "true"
+  }
+}
+
+resource "lxd_profile" "myprofile" {
+  name = "myprofile"
+  config = {
+    "limits.cpu"             = 2
+    "limits.memory"          = "2GiB"
+    "cloud-init.vendor-data" = "${data.template_file.cloudinit_file.rendered}"
+  }
+  device {
+    name = "eth0"
+    type = "nic"
+    properties = {
+      network = "${lxd_network.mynetwork.name}"
+    }
+  }
+}
+
+resource "lxd_container" "freshy" {
+  name     = "freshy"
+  image    = "ubuntu:22.04"
+  type     = "virtual-machine"
+  profiles = ["default", "${lxd_profile.myprofile.name}"]
+}
